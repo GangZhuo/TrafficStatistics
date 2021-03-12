5c08cb80dcdad9183c2949eb801d38c18c34872d
Gang Zhuo <gang.zhuo@gmail.com>
2021-03-12 15:53:01 +0800

    Tray Icon

--- TrafficStatistics/Program.cs
+++ TrafficStatistics/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,6 +9,11 @@ namespace TrafficStatistics
 {
     static class Program
     {
+        static NotifyIcon _notifyIcon;
+        static ContextMenu contextMenu1;
+        static TrafficStatisticsForm form;
+        static bool closing;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +22,67 @@ namespace TrafficStatistics
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TrafficStatisticsForm());
+            CreateContextMenu();
+            CreateTrayIcon();
+            form = new TrafficStatisticsForm();
+            form.FormClosing += MainForm_FormClosing;
+            form.Show();
+            form.Activate();
+            Application.Run();
         }
+
+        static void CreateContextMenu()
+        {
+            contextMenu1 = new ContextMenu(new MenuItem[] {
+                new MenuItem("Open", new EventHandler(OnOpenMainFormMenuItemClick)),
+                new MenuItem("-"),
+                new MenuItem("Quit", new EventHandler(OnQuitMenuItemClick))
+            });
+        }
+
+        static void CreateTrayIcon()
+        {
+            _notifyIcon = new NotifyIcon();
+            _notifyIcon.DoubleClick += OnNotifyIconDoubleClick;
+            _notifyIcon.BalloonTipClicked += OnBalloonTipClicked;
+            _notifyIcon.Icon = Properties.Resources.wave;
+            _notifyIcon.Text = "Traffic Statistics";
+            _notifyIcon.Visible = true;
+            _notifyIcon.ContextMenu = contextMenu1;
+        }
+
+        static void OnNotifyIconDoubleClick(object sender, EventArgs e)
+        {
+            form.Show();
+            form.Activate();
+        }
+
+        static void OnBalloonTipClicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        static void OnOpenMainFormMenuItemClick(object sender, EventArgs e)
+        {
+            form.Show();
+            form.Activate();
+        }
+
+        static void OnQuitMenuItemClick(object sender, EventArgs e)
+        {
+            closing = true;
+            form.Close();
+            Application.Exit();
+        }
+
+        private static void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!closing)
+            {
+                e.Cancel = true;
+                form.Hide();
+            }
+        }
+
     }
 }
